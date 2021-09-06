a3385299296c754ff0a131005ee0999ee0138b9e
jmbenedetto <jmbenedetto@gmail.com>
2021-09-06 00:54:05 -0300

    calculate engagement with manual user extraction

--- notebooks/2.3_Prepare ft_users_engagement source gbq users_manual.py
+++ notebooks/2.3_Prepare ft_users_engagement source gbq users_manual.py
@@ -35,16 +35,13 @@ import logging
 # # PARAMETERS
 
 # %%
-logger = logging.getLogger("pandas_gbq")
-logger.setLevel(logging.DEBUG)
-logger.addHandler(logging.StreamHandler())
-
 project_id = "analytics-dev-308300"
 
-credentials = service_account.Credentials.from_service_account_file(
-    "../keys/gcp_key.json",
-)
+cred_file="../keys/gcp_key.json"
 
+destination_table='raw_engagement.users_engagement'
+
+# %%
 pd.set_option("display.max_rows", 200)
 pd.set_option("display.max_columns", None)
 pd.set_option("display.width", None)
@@ -58,14 +55,16 @@ pd.set_option("display.max_colwidth", None)
 # ## get data
 
 # %%
-def get_data():
+def get_data(project_id, credentials):
     """
     ()-->df
     """
     query = """
         SELECT DISTINCT
-            user_id, created_at, extraction_date,
-            CONCAT(first_name, ' ', last_name) as user_name
+            user_id,
+            created_at,
+            extraction_date,
+            user_name
         FROM
             dtm_engagement.dim_users
         WHERE
@@ -84,7 +83,10 @@ def get_data():
 
     query = """
         SELECT DISTINCT
-            user_id, last_login, extraction_date
+            user_id,
+            days_since_last_login,
+            extraction_date,
+            joined_group
         FROM
             dtm_engagement.hist_users
         WHERE
@@ -98,9 +100,9 @@ def get_data():
 
     login_df=login_df.drop_duplicates(subset=['user_id','extraction_date'],keep='last',ignore_index=True)
     
-    login_df["last_login"] = pd.to_datetime(
-        login_df["last_login"], utc=True
-    )
+#     login_df["last_login"] = pd.to_datetime(
+#         login_df["last_login"], utc=True
+#     )
 
     query = """
         SELECT DISTINCT
@@ -132,7 +134,7 @@ def get_data():
 
 # %%
 def create_base_df(
-    ls_users,
+    users_ls,
     creation_df,
     start_date="2021-08-16",
     end_date=pd.Timestamp.today().strftime("%Y-%m-%d"),
@@ -152,7 +154,7 @@ def create_base_df(
     actions_dict = [
         {"action_date": action_date, "user_id": user}
         for action_date in dates_index
-        for user in ls_users
+        for user in users_ls
     ]
     
     base_df=pd.DataFrame(actions_dict)
@@ -168,7 +170,7 @@ def create_base_df(
 # %%
 def max_date(consumption_df, reporting_date, user_id, date_of_interest):
     """
-    (df,date like str, int)
+    (df,date like str, int)-->date
     Select the maximum value for date_of_interest field that is inferior to the reporting date (23:59:59), for the specified user_id.
     """
     max_start = consumption_df[
@@ -188,7 +190,7 @@ def max_date(consumption_df, reporting_date, user_id, date_of_interest):
 # %%
 def calculate_nb_of_sets_of_interest(consumption_df,reporting_date,user_id,date_of_interest,nb_of_days):
     """
-    (df, date like str, int, int, date like str)-->number
+    (df, date like str, int, int, date like str)-->int
     For the user_id, count the number of set_ids where completed at is between reporting_date-number_of_days and reporting_date.
     """
 
@@ -202,56 +204,56 @@ def calculate_nb_of_sets_of_interest(consumption_df,reporting_date,user_id,date_
 # ## Set user status based on dates
 
 # %%
-def user_status(timedelta_since_last_login, timedelta_since_last_start,timedelta_since_last_completion,ever_logged):
+def user_status(days_since_last_login, days_since_last_start,days_since_last_completion,ever_logged):
     """
     (timedelta,timedelta,timedelta,bool)--> str
     """
-    if not ever_logged:
+    if ever_logged==False:
         return '0.bird'
-    elif timedelta_since_last_completion <= pd.Timedelta(7,'D'):
+    elif days_since_last_completion <= 7:
         return '4.learner'
-    elif timedelta_since_last_start <= pd.Timedelta(7,'D'):
+    elif days_since_last_start <= 7:
         return '3.consumer'
-    elif timedelta_since_last_login <= pd.Timedelta(7,'D'):
+    elif days_since_last_login <= 7:
         return '2.curious'
     else:
         return '1.missing'
 
-assert user_status(
-    timedelta_since_last_login=pd.Timedelta(pd.NaT),
-    timedelta_since_last_start=pd.Timedelta('2 days 13:41:36'),
-    timedelta_since_last_completion=pd.Timedelta('1 days 11:29:23'),
-    ever_logged=True)=='4.learner'
+# assert user_status(
+#     timedelta_since_last_login=pd.Timedelta(pd.NaT),
+#     timedelta_since_last_start=pd.Timedelta('2 days 13:41:36'),
+#     timedelta_since_last_completion=pd.Timedelta('1 days 11:29:23'),
+#     ever_logged=True)=='4.learner'
 
-assert user_status(
-    timedelta_since_last_login=pd.Timedelta(pd.NaT),
-    timedelta_since_last_start=pd.Timedelta('1 days 13:41:36'),
-    timedelta_since_last_completion=pd.Timedelta('7 days 11:29:23'),
-    ever_logged=True)=='3.consumer'
+# assert user_status(
+#     timedelta_since_last_login=pd.Timedelta(pd.NaT),
+#     timedelta_since_last_start=pd.Timedelta('1 days 13:41:36'),
+#     timedelta_since_last_completion=pd.Timedelta('7 days 11:29:23'),
+#     ever_logged=True)=='3.consumer'
 
-assert user_status(
-    timedelta_since_last_login=pd.Timedelta(pd.NaT),
-    timedelta_since_last_start=pd.Timedelta('1 days 13:41:36'),
-    timedelta_since_last_completion=pd.Timedelta(pd.NaT),
-    ever_logged=True)=='3.consumer'
+# assert user_status(
+#     timedelta_since_last_login=pd.Timedelta(pd.NaT),
+#     timedelta_since_last_start=pd.Timedelta('1 days 13:41:36'),
+#     timedelta_since_last_completion=pd.Timedelta(pd.NaT),
+#     ever_logged=True)=='3.consumer'
 
-assert user_status(
-    timedelta_since_last_login=pd.Timedelta('1 days 13:41:36'),
-    timedelta_since_last_start=pd.Timedelta('8 days 13:41:36'),
-    timedelta_since_last_completion=pd.Timedelta('7 days 11:29:23'),
-    ever_logged=True)=='2.curious'
+# # assert user_status(
+# #     timedelta_since_last_login=pd.Timedelta('1 days 13:41:36'),
+# #     timedelta_since_last_start=pd.Timedelta('8 days 13:41:36'),
+# #     timedelta_since_last_completion=pd.Timedelta('7 days 11:29:23'),
+# #     ever_logged=True)=='2.curious'
 
-assert user_status(
-    timedelta_since_last_login=pd.Timedelta('7 days 13:41:36'),
-    timedelta_since_last_start=pd.Timedelta('8 days 13:41:36'),
-    timedelta_since_last_completion=pd.Timedelta('10 days 11:29:23'),
-    ever_logged=True)=='1.missing'
+# assert user_status(
+#     timedelta_since_last_login=pd.Timedelta('7 days 13:41:36'),
+#     timedelta_since_last_start=pd.Timedelta('8 days 13:41:36'),
+#     timedelta_since_last_completion=pd.Timedelta('10 days 11:29:23'),
+#     ever_logged=True)=='1.missing'
 
-assert user_status(
-    timedelta_since_last_login=pd.Timedelta('0 days'),
-    timedelta_since_last_start=pd.Timedelta('0 days'),
-    timedelta_since_last_completion=pd.Timedelta('0 days'),
-    ever_logged=False)=='0.bird'
+# assert user_status(
+#     timedelta_since_last_login=pd.Timedelta('0 days'),
+#     timedelta_since_last_start=pd.Timedelta('0 days'),
+#     timedelta_since_last_completion=pd.Timedelta('0 days'),
+#     ever_logged=False)=='0.bird'
 
 
 # %% [markdown]
@@ -260,35 +262,57 @@ assert user_status(
 # %%
 def generate_engagement_df(base_df,consumption_df,login_df):
     """
-    (df,df)-->df
-    Update the start_date for each user with the maximum value inferior to the action_date. Action date is extended with 23:59:59 to encompass the entire day.
+    (df,df,df)-->df
     """
     engagement_df=base_df.copy()
-
-    engagement_df['ever_logged']=engagement_df.apply(lambda x: max_date(login_df,x['action_date'],x['user_id'],'last_login'), axis=1)
     
-    engagement_df['last_login_date']=engagement_df.apply(lambda x: max_date(login_df,x['action_date'],x['user_id'],'last_login'), axis=1)
-    engagement_df['timedelta_since_last_login']=pd.to_datetime(engagement_df['action_date']+' 23:59:59',utc=True)-pd.to_datetime(engagement_df['last_login_date'],utc=True)
-    engagement_df['days_since_last_login']=engagement_df['timedelta_since_last_login'].dt.days
+    engagement_df=pd.merge(engagement_df,login_df,left_on=['user_id','action_date'],right_on=['user_id','extraction_date'])
     
     engagement_df['last_start_date']=engagement_df.apply(lambda x: max_date(consumption_df,x['action_date'],x['user_id'],'started_at'), axis=1)
     engagement_df['timedelta_since_last_start']=pd.to_datetime(engagement_df['action_date']+' 23:59:59',utc=True)-engagement_df['last_start_date']
     engagement_df['days_since_last_start']=engagement_df['timedelta_since_last_start'].dt.days
     
     engagement_df['last_completion_date']=engagement_df.apply(lambda x: max_date(consumption_df,x['action_date'],x['user_id'],'completed_at'), axis=1)
-    engagement_df['timedelta_since_last_completion']=pd.to_datetime(engagement_df['action_date']+' 23:59:59',utc=True)-engagement_df['last_completion_date']
+    engagement_df['timedelta_since_last_completion']=(
+        pd.to_datetime(engagement_df['action_date']+' 23:59:59',utc=True)-engagement_df['last_completion_date']
+            )
     engagement_df['days_since_last_completion']=engagement_df['timedelta_since_last_completion'].dt.days
     
     engagement_df['nb_of_completed_sets']=engagement_df.apply(lambda x: calculate_nb_of_sets_of_interest(consumption_df=consumption_df,reporting_date=x['action_date'],user_id=x['user_id'],date_of_interest='completed_at',nb_of_days=7),axis=1)
     
     engagement_df['user_status']=engagement_df.apply(lambda x:
                                                                user_status(
-                                                                   x['timedelta_since_last_login'],
-                                                                   x['timedelta_since_last_start'],
-                                                                   x['timedelta_since_last_completion'],
-                                                                   ever_logged=True),
+                                                                   x['days_since_last_login'],
+                                                                   x['days_since_last_start'],
+                                                                   x['days_since_last_completion'],
+                                                                   ever_logged=x['joined_group']),
                                                                axis=1
                                                               )
+    
+    return engagement_df
+
+
+# %% [markdown]
+# ## create user_engagement table in gbq
+
+# %%
+def create_engagement_table(project_id=project_id, destination_table_name=destination_table, cred_file=cred_file):
+    """
+    (df,str,str,str)--> gbq table
+    """
+    
+    credentials = service_account.Credentials.from_service_account_file(cred_file)
+    
+    users_ls, creation_df, login_df, consumption_df = get_data(project_id=project_id,credentials=credentials)
+    
+    engagement_df = generate_engagement_df(
+                                create_base_df(users_ls=users_ls,
+                                               creation_df=creation_df),
+                                consumption_df=consumption_df,
+                                login_df=login_df
+                    )
+    
+    engagement_df.to_gbq(destination_table_name,project_id=project_id,credentials=credentials,if_exists='replace')
     
     return engagement_df
 
@@ -297,28 +321,13 @@ def generate_engagement_df(base_df,consumption_df,login_df):
 # # DATA WRANGLING
 
 # %%
-ls_users, df_creation, df_login, df_consumption = get_data()
+create_engagement_table()
 
 # %%
-df_engagement = generate_engagement_df(create_base_df(ls_users,df_creation),df_consumption,df_login)
-df_engagement
+credentials = service_account.Credentials.from_service_account_file(cred_file)
+a,b,c,d = get_data(project_id=project_id, credentials=credentials)
 
 # %%
-df_engagement.to_gbq('raw_engagement.users_engagement',project_id=project_id,if_exists='replace',credentials=credentials)
-
-# %%
-df_login.head(5)
-
-# %%
-df_log['loggin_1st']=df_log.groupby(by=['user_id'])['last_login'].transform(min)
-
-# %%
-df_log[df_log['user_id']==20165]
-
-# %%
-df_login['extraction_date'].max()
-
-# %%
-df_login['last_login'].max()
+c['timedelta_since_last_login'].to_datetime()
 
 # %%
